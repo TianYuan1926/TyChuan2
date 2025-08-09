2f6350ebc6e3c35d4cc609f5a68ebc49b7853426
TianYuan1926 <17651763538@163.com>
2025-08-10 04:33:06 +0800

    Add files via upload

--- config/config.js
+++ config/config.js
@@ -1,7 +1,13 @@
+// Step3：预填你的 Supabase 与 hCaptcha
 window.APP_CONFIG = {
   SUPABASE_URL: "https://lvoawckqgfbdpxufkabx.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2b2F3Y2txZ2ZiZHB4dWZrYWJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3NjQxMzYsImV4cCI6MjA3MDM0MDEzNn0.b7VmnydzPltH0Z__tRBmtOXRGzlI718CFruSDs95px8",
-  VERSION: "P0-AuthSecure-Step2-r1",
+  VERSION: "P0-AuthSecure-Step3",
   ENV: "dev",
   HCAPTCHA_SITEKEY: "92d48fee-72fc-481a-a0e6-690084662786"
+};
+
+// hCaptcha 稳定渲染 onLoad 回调
+window.hcaptchaOnLoad = function(){
+  if(window.__renderCaptchas) window.__renderCaptchas();
 };
