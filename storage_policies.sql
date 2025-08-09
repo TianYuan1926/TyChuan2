96fc644b3bd8417664288776b4b9dbec45ef8ab4
TianYuan1926 <17651763538@163.com>
2025-08-10 02:45:40 +0800

    Delete storage_policies.sql

--- storage_policies.sql
+++
@@ -1,9 +0,0 @@
--- storage_policies.sql
-drop policy if exists "receipt_read"   on storage.objects;
-drop policy if exists "receipt_insert" on storage.objects;
-drop policy if exists "receipt_update" on storage.objects;
-drop policy if exists "receipt_delete" on storage.objects;
-create policy "receipt_read"   on storage.objects for select using (bucket_id = 'receipts');
-create policy "receipt_insert" on storage.objects for insert with check (bucket_id = 'receipts');
-create policy "receipt_update" on storage.objects for update using (bucket_id = 'receipts');
-create policy "receipt_delete" on storage.objects for delete using (bucket_id = 'receipts');
